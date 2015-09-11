3fe3d721fbf4a5074a07ecd13612c18b3eecbe9e
Aakash <aakash.aggarwal@practo.com>
2015-09-11 16:57:36 +0530

    Closed #14 and #15

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def index
+  end
+
+  def doctorNextStep
+  end
+
+  def hospitalNextStep
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#index'
+
+  get 'next-doctor' => 'static#doctorNextStep'
+  get 'next-hospital' => 'static#hospitalNextStep'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
